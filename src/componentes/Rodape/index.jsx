49de4a086fe479cddca7367d01cd73dc39b7744c
Matheus <mat.hen.gra@gmail.com>
2023-04-27 11:47:52 -0300

    ajustes rodape

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -8,9 +8,9 @@ export default function Rodape() {
   return (
     <footer className={styles.rodape} >
         <div className={styles.rodape__icones} >
-            <a href="https://www.facebook.com/" target='_blank' rel='noreferrer' ><img src={facebook} alt='logo do Facebook' /></a>
-            <a href="https://twitter.com/" target='_blank' rel='noreferrer' ><img src={twitter} alt='logo do Twitter' /></a>
-            <a href="https://www.instagram.com/" target='_blank' rel='noreferrer' ><img src={instagram} alt='logo do Instagram' /></a>
+            <a href="https://www.facebook.com/" target='_blank' rel='noreferrer' ><img src={facebook} alt='ícone do Facebook' /></a>
+            <a href="https://twitter.com/" target='_blank' rel='noreferrer' ><img src={twitter} alt='ícone do Twitter' /></a>
+            <a href="https://www.instagram.com/" target='_blank' rel='noreferrer' ><img src={instagram} alt='ícone do Instagram' /></a>
         </div>
         <p>Desenvolvido por Alura.</p>
     </footer>
